1c5f8d5090a06d98f5d183b00a13b3c4468cc642
charlie <70538109+cbelinky@users.noreply.github.com>
2020-10-24 15:15:06 -0400

    lab6 final commit
    final commit

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,9 @@
-// You may wish to find an effective randomizer function on MDN.
+/* eslint-disable linebreak-style */
+
+function getRandomInt(max) {
+  // src: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function range(int) {
   const arr = [];
@@ -8,10 +13,11 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
+function sortByKey(a, b, key) {
   if (a[key] < b[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  }
+  if (a[key] > b[key]) {
     return 1;
   }
   return 0;
@@ -23,14 +29,35 @@ document.body.addEventListener('submit', async (e) => {
   fetch('/api', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(form)
+    body: JSON.stringify(form),
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const myArr = range(10);
+      const myArr1 = myArr.map(() => {
+        const num = getRandomInt(243); // uses 243 to match the number of countries in the JSON file
+        return fromServer[num];
+      });
+
+      const reverseList = myArr1.sort((a, b) => sortByKey(b, a, 'name'));
+      const finalList = document.createElement('ol');
+      finalList.className = 'flex-inner';
+      $('form').append(finalList);
+
+      reverseList.forEach((lmnt, i) => {
+        const li = document.createElement('li');
+        $(li).append(
+          `<input type = "checkbox" value = ${lmnt.code} id = ${lmnt.code} />`
+        );
+        $(li).append(`<label for = ${lmnt.code}> ${lmnt.name}</label>`);
+        $(finalList).append(li);
+      });
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
