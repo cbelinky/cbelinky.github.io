17405bd3afd2d678fa76ac750bc2ce9abb6afb57
charlie <70538109+cbelinky@users.noreply.github.com>
2020-10-30 22:00:27 -0400

    lab7 final
    passes all cypress tests

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,28 +1,44 @@
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
-  return list;
+  const myArr = [];
+  const myObj = {};
+  for (let i = 0; i < restaurantList.length; i += 1) {
+    myArr.push(restaurantList[i].category);
+  }
+  for (let i = 0; i < myArr.length; i += 1) {
+    if (!myObj[myArr[i]]) {
+      myObj[myArr[i]] = 0;
+    }
+    myObj[myArr[i]] += 1;
+  }
+  const formatedList = Object.keys(myObj).map((category) => ({
+    y: myObj[category],
+    label: category,
+  }));
+  return formatedList;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  CanvasJS.addColorSet('customColorSet1', [ // colorSet Array
+  CanvasJS.addColorSet('customColorSet1', [
+    // colorSet Array
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
-    '#89B3D9',
-    '#418FBF',
-    '#F2E7C4',
-    '#A67041',
-    '#F24130',
+    '#3E454C',
+    '#2185C5',
+    '#7ECEFD',
+    '#FFF6E5',
+    '#FF7F66',
   ]);
 
   return {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'places to eat out in the future'
+      text: 'Places To Eat Out In Future',
     },
     axisX: {
       interval: 1,
-      labelFontSize: 12
+      labelFontSize: 12,
     },
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
@@ -30,21 +46,22 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
       title: 'retaurants by category',
       labelFontSize: 12,
       scaleBreaks: {
-        customBreaks: [ // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+        customBreaks: [
+          // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
           { startValue: 40, endValue: 50, color: '#EB8CC6' },
           { startValue: 85, endValue: 100, color: '#3EA0DD' },
           { startValue: 140, endValue: 175, color: '#EC5657' },
         ],
-      }, 
+      },
     },
     data: [
       {
         type: 'bar',
         name: 'restaurants',
         axisYType: 'secondary',
-        dataPoints: datapointsFromRestaurantsList
-      }
-    ]
+        dataPoints: datapointsFromRestaurantsList,
+      },
+    ],
   };
 }
 
@@ -67,9 +84,9 @@ document.body.addEventListener('submit', async (e) => {
   fetch('/api', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(form)
+    body: JSON.stringify(form),
   })
     .then((fromServer) => fromServer.json())
     .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async(req, res) => {
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
